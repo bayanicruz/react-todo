0d9e0cff2deb8404135d6db59af38c3ca45fc0b0
bacruz <bayani.cruz@gmail.com>
2023-08-22 19:00:16 +1000

    Initial commit - frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import Todo from "./components/todo"
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="container">
+        <h1>todo app</h1>
+
+        <div className="top">
+          <input type="text" placeholder="Add todos" />
+          <div className="add">Add</div>
+        </div>
+
+       
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
